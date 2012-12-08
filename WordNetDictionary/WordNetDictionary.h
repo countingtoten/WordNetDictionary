8e9b90371ca550d8d81b0bd47dc4e7be8ccd0f40
Jim <countingtoten@gmail.com>
2012-12-08 03:06:52 -0600

    Original version of library

--- WordNetDictionary/WordNetDictionary.h
+++ WordNetDictionary/WordNetDictionary.h
@@ -10,4 +10,16 @@
 
 @interface WordNetDictionary : NSObject
 
++ (WordNetDictionary *)sharedInstance;
+
+// Returns
+- (NSArray *)searchForWord:(NSString *)searchText;
+- (NSArray *)searchForWord:(NSString *)searchText withLimit:(int)limit;
+
+// Returns a dictionary of definitions. The keys are the part of speech and can be
+// @"noun", @"verb", @"adjective", or @"adverb"
+- (NSDictionary *)defineWord:(NSString *)wordToDefine;
+
+- (NSArray *)randomWords:(int )limit;
+
 @end
